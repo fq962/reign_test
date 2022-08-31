110f492d8e090ecdeb5c387ca71eb6caecdea1d4
Fernando Quintanilla <df.quintanillah@gmail.com>
2022-08-31 13:13:52 -0600

    conexion con MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 // import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NodeNewsModule } from './modules/node-news/node-news.module';
 
 @Module({
-  imports: [NodeNewsModule],
+  imports: [
+    NodeNewsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
